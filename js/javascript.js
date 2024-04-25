2eb4fa4c614d78504c84122eee01afd264181aac
Callum Glass <40589677@live.napier.ac.uk>
2024-04-25 19:28:14 +0100

    update questions

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,3 @@
-function check_url(){
-
-    const searchParams = new URLSearchParams(window.location.search);
-    console.log(searchParams.has('sort')); // true
-    console.log(searchParams.get('sort')); // price_descending
-
-
-}
-
-function create_user_link(){
-
-    var forename = document.getElementById("forename").value;
-    var surname = document.getElementById("surname").value;
-    var quiz1_link = document.getElementById("quiz1_link");
-
-    const searchParams = new URLSearchParams(window.location.search);
-    var forename = searchParams.get('forename'); 
-    var surname = searchParams.get('surname'); 
-
-    var getUrl = window.location.href;
-    var baseUrl = getUrl.replace("leaderboard.html", "");
-
-    if (forename == "" || surname == ""){
-        document.getElementById("username_error").innerHTML = "Please enter your forename";
-    } else{
-        quiz1_link.href = baseUrl + "quiz1.html?forename="+ forename +"&surname="+ surname;
-        quiz2_link.href = baseUrl + "quiz2.html?forename="+ forename +"&surname="+ surname;
-        quiz3_link.href = baseUrl + "quiz3.html?forename="+ forename +"&surname="+ surname;
-        quiz4_link.href = baseUrl + "quiz4.html?forename="+ forename +"&surname="+ surname;
-    }
-
-}
-
 function mark_quiz1(){
 
     var question1 = document.getElementById("quiz1_question1").value;
@@ -143,32 +110,32 @@ function mark_quiz3(){
 
 function mark_quiz4(){
 
-    var question1 = document.getElementById("quiz1_question1").value;
-    var question2 = document.getElementById("quiz1_question2").value;
-    var question3 = document.getElementById("quiz1_question3").value;
-    var question4 = document.getElementById("quiz1_question4").value;
-    var question5 = document.getElementById("quiz1_question5").value;
+    var question1 = document.getElementById("quiz4_question1").value;
+    var question2 = document.getElementById("quiz4_question2").value;
+    var question3 = document.getElementById("quiz4_question3").value;
+    var question4 = document.getElementById("quiz4_question4").value;
+    var question5 = document.getElementById("quiz4_question5").value;
 
-    var quiz1_result = document.getElementById("quiz1_results_text");
+    var quiz4_result = document.getElementById("quiz4_results_text");
 
     var score = 0;
 
-    if (question1 == "Q1"){
+    if (question1 == "Mike Hawthorn"){
         score ++;
     }
-    if (question2 == "Q2"){
+    if (question2 == "Max Verstappen"){
         score ++;
     }
-    if (question3 == "Q3"){
+    if (question3 == "Cars"){
         score ++;
     }
-    if (question4 == "Q4"){
+    if (question4 == "Tesla"){
         score ++;
     }
-    if (question5 == "Q5"){
+    if (question5 == "Ford Puma"){
         score ++;
     }
 
-    quiz1_result.innerHTML = "You scored " + score + " out of 5";
+    quiz4_result.innerHTML = "You scored " + score + " out of 5";
     
 }
